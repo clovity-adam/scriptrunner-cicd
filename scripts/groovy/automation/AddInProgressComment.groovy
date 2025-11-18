812c6a8cc3510bac71e3fe8c2b77f79c7f2c8c65
Adam Martin <adamm@clovity.com>
2025-11-18 15:10:47 -0500

    add new script for automation rule

---
+++ scripts/groovy/automation/AddInProgressComment.groovy
@@ -0,0 +1,17 @@
+// scripts/groovy/automation/AddInProgressComment.groovy
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+
+// In ScriptRunner's Automation action, "issue" is injected into the binding
+def Issue iss = issue as Issue
+
+def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+def commentManager = ComponentAccessor.commentManager
+
+def msg = "SR demo: issue moved to In Progress at ${new Date()}"
+
+// Add a comment
+commentManager.create(iss, user, msg, false)
+
+// Log for debugging
+log.warn("[SR-DEV A4J] Added comment to ${iss.key}: ${msg}")
